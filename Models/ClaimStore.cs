52c6da3e460acb026cb172d40aca22bd47974ecb
marvins56 <82571414+marvins56@users.noreply.github.com>
2022-11-07 23:48:57 +0300

    user claims created

---
+++ Models/ClaimStore.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+
+namespace STMIS.Models
+{
+    public class ClaimStore
+    {
+
+        public static List<Claim> claimsList = new List<Claim>()
+        {
+            new Claim("Create", "Create"),
+            new Claim("Edit", "Edit"),
+            new Claim("Delete", "Delete")
+        };
+    }
+}
